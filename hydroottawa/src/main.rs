001f2aa0dd3faa17b359435cbce348ff2d3da3f5
uintptr <uintptr@randomail.ca>
2026-08-22 11:41:54 -0400

    added password command

--- hydroottawa/src/main.rs
+++ hydroottawa/src/main.rs
@@ -1,6 +1,6 @@
 mod display;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use chrono::{Local, NaiveDate};
 use clap::Parser;
 use dialoguer::Password;
@@ -8,7 +8,8 @@ use hydroottawa::mqtt_pub::mqtt_publish;
 use hydroottawa_api::{api::HoApi, auth::HoAuth};
 use log::LevelFilter;
 use rstaples::logging::StaplesLogger;
-use std::env;
+use std::{env, process::Command};
+use which::which;
 
 use display::{ProfileDisplay, UsageDisplay};
 
@@ -33,17 +34,38 @@ struct UserArgs {
     date: NaiveDate,
 
     /// Username
-    #[arg(short, long)]
+    #[arg(short, long, env = "HO_USERNAME")]
     username: String,
+
+    /// Password Command
+    #[arg(short, long, env = "HO_PASSWORD_COMMAND")]
+    password_command: Option<String>,
 
     /// Username
     #[arg(short, long)]
     mqtt: Option<String>,
 }
 
-fn get_password(username: &str) -> Result<String> {
+fn get_password_from_command(command: &str) -> Result<String> {
+    let args =
+        shell_words::split(command).with_context(|| format!("unable to tokeniz {command}"))?;
+
+    let (command, args) = args.split_first().context("couple split command")?;
+
+    let command = which(command).with_context(|| format!("{command} was not found in PATH"))?;
+
+    let output = Command::new(command).args(args).output()?;
+
+    String::from_utf8(output.stdout).context("output not utf-8")
+}
+
+fn get_password(username: &str, password_command: &Option<String>) -> Result<String> {
     if let Ok(password) = env::var("HO_PASSWORD") {
         Ok(password)
+    } else if let Some(command) = password_command {
+        get_password_from_command(&command)
+    } else if let Ok(command) = env::var("HO_PASSWORD_COMMAND") {
+        get_password_from_command(&command)
     } else {
         let prompt = format!("Password for {username}");
         let password = Password::new().with_prompt(prompt).interact()?;
@@ -63,7 +85,7 @@ async fn main() -> Result<()> {
 
     StaplesLogger::new().with_colors().with_log_level(log_level).start();
 
-    let password = get_password(&args.username)?;
+    let password = get_password(&args.username, &args.password_command)?;
 
     let auth = HoAuth::new(&args.username, &password).await?;
     println!("Authentication successful!");
