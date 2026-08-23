8b8df0824f1ee0ffa2aa6f56e32db52439ca3ea3
uintptr <uintptr@randomail.ca>
2026-08-23 18:21:40 -0400

    looping

--- hydroottawa/src/spoon_feed.rs
+++ hydroottawa/src/spoon_feed.rs
@@ -1,9 +1,28 @@
-use anyhow::Result;
+use std::{thread::sleep, time::Duration};
+
+use anyhow::{Result, bail};
 use hydroottawa_api::{
     api::{DebugResponses, HoApi},
     auth::HoAuth,
 };
 use log::info;
+
+const AUTH_ATTEMPTS: usize = 5;
+
+fn auth_loop<U, P>(user: U, password: P) -> Result<HoAuth>
+where
+    U: AsRef<str>,
+    P: AsRef<str>,
+{
+    for _ in 0..AUTH_ATTEMPTS {
+        if let Ok(auth) = HoAuth::new(&user, &password) {
+            return Ok(auth);
+        }
+        sleep(Duration::from_secs(10));
+    }
+
+    bail!("Unable to authenticate");
+}
 
 pub fn spoon_feed<U, P, M>(user: U, password: P, _mqtt_server: M) -> Result<()>
 where
@@ -13,9 +32,8 @@ where
 {
     let _api = HoApi::new(DebugResponses::Off);
 
-    let _auth = HoAuth::new(user, password)?;
-
-    info!("authenticated");
-
-    Ok(())
+    loop {
+        let _auth = auth_loop(&user, &password)?;
+        info!("authenticated");
+    }
 }
